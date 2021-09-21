304da80bc511bb3af1693c11913ec02868988233
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-21 15:16:00 -0300

    feat(Home): add page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss"
+
+interface Props {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: Props) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
+}
