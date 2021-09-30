470a22afa6a6ee88532e8ef9303686ea34c966c5
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-30 09:07:19 -0300

    test(Header): ensure behavior

--- src/components/ActiveLink/index.spec.tsx
+++ src/components/ActiveLink/index.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from "@testing-library/react"
+import { render, screen} from "@testing-library/react"
 import { ActiveLink } from '.';
 
 jest.mock("next/router", () => {
@@ -10,22 +10,22 @@ jest.mock("next/router", () => {
 
 describe('ActiveLink', () => {
   it('should render without the active class', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/any" activeClassName="any_class">
         <div>Home</div>
       </ActiveLink>
     );
 
-    expect(getByText("Home")).toBeInTheDocument()
-    expect(getByText("Home")).not.toHaveClass("any_class")
+    expect(screen.getByText("Home")).toBeInTheDocument()
+    expect(screen.getByText("Home")).not.toHaveClass("any_class")
   });
   it('should render with the active class', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="any_class">
         <div>Home</div>
       </ActiveLink>
     );
 
-    expect(getByText("Home")).toHaveClass("any_class")
+    expect(screen.getByText("Home")).toHaveClass("any_class")
   });
 });
---
+++ src/components/Header/index.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { screen, render } from "@testing-library/react"
+import { Header } from '.';
+
+jest.mock("next/router", () => {
+  return {
+    useRouter: () => ({ asPath: "/" })
+  }
+})
+
+jest.mock("next-auth/client", () => {
+  return {
+    useSession: () => [null, false]
+  }
+})
+
+describe('Header', () => {
+  it('should render', () => {
+    render(
+      <Header />
+    );
+
+    expect(screen.getByText("Home")).toBeInTheDocument()
+    expect(screen.getByText("Posts")).toBeInTheDocument()
+  });
+});
