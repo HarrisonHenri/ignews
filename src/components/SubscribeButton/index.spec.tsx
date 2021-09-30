7bbc8a3207527c1435cca754b149075a3dae4197
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-30 10:02:57 -0300

    test(SubscribeButton): ensure behavior

---
+++ src/components/SubscribeButton/index.spec.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { screen, render, fireEvent } from "@testing-library/react"
+import { SubscribeButton } from '.';
+import { mocked } from "ts-jest/utils";
+import { signIn, useSession } from 'next-auth/client';
+import {useRouter} from 'next/router';
+
+jest.mock("next-auth/client")
+
+jest.mock("next/router")
+
+describe('SubscribeButton', () => {
+  it('should render', () => {
+    const mockedUseSession = mocked(useSession)
+    mockedUseSession.mockReturnValueOnce([null, false])
+
+    render(
+      <SubscribeButton />
+    );
+
+    expect(screen.getByText("Subscribe now")).toBeInTheDocument()
+  });
+  
+  it('should navigate the user to signin when is not authenticated', () => {
+    const mockedSignIn = mocked(signIn)
+    const mockedUseSession = mocked(useSession)
+    mockedUseSession.mockReturnValueOnce([null, false])
+
+    render(
+      <SubscribeButton />
+    );
+
+    const subscribeButton = screen.getByText("Subscribe now")
+
+    fireEvent.click(subscribeButton)
+
+    expect(mockedSignIn).toBeCalled()
+  });
+
+  fit('should navigate the user to posts when the user has a subscription', () => {
+    const mockedUseSession = mocked(useSession)
+    mockedUseSession.mockReturnValueOnce([{activeSubscription: true}, false])
+
+    const pushMock = jest.fn()
+    const mockedRouter = mocked(useRouter)
+    mockedRouter.mockReturnValueOnce({
+      push: pushMock
+    } as any)
+
+    render(
+      <SubscribeButton />
+    );
+
+    const subscribeButton = screen.getByText("Subscribe now")
+
+    fireEvent.click(subscribeButton)
+
+    expect(pushMock).toBeCalled()
+  });
+});
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,20 +1,23 @@
 import { signIn, useSession } from "next-auth/client"
+import { useRouter } from "next/router"
 import { useCallback } from "react"
 import { api } from "../../services/api"
 import { getStripeJs } from "../../services/stripe-js"
 import styles from "./styles.module.scss"
 
-interface Props {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: Props) {
+export function SubscribeButton() {
   const [session] = useSession()
+  const router = useRouter()
 
   const handleSubscribe = useCallback(
     async () => {
       if (!session){
         signIn("github")
+        return;
+      }
+
+      if (session.activeSubscription){
+        router.push("/posts")
         return;
       }
 
@@ -30,7 +33,7 @@ export function SubscribeButton({ priceId }: Props) {
         alert(error.message)
       }
     },
-    [session],
+    [session, router],
   )
 
   return (
